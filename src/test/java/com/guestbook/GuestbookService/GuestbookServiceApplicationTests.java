4eb6e3dbfad37030d40bd0372c558feb00394ce1
bharchan2607 <bharathi.muralinathan@cognizant.com>
2021-02-09 11:00:08 -0600

    Project Setup

---
+++ src/test/java/com/guestbook/GuestbookService/GuestbookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guestbook.GuestbookService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestbookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
